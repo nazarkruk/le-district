f645a4f6e38fc6c3b153ade6bdc73d97ed571e46
Your NameNazar <nazarkruk@icloud.com>
2022-10-10 00:29:06 -0400

    adding contact us

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(function () {
+    console.log("test")
+
+    function initMap() {
+
+        var location = new google.maps.LatLng(50.0875726, 14.4189987);
+
+
+    }
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+})
